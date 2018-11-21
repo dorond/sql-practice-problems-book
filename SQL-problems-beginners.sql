bd697f77cd0a0879d64a91475586edeaf24bd43d
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-11-21 14:53:30 +0200

    completed up to question 16

--- SQL-problems-beginners.sql
+++ SQL-problems-beginners.sql
@@ -4,42 +4,86 @@ SELECT * FROM Shippers
 
 /* Q2 */
 
-SELECT CategoryName, Description FROM Categories
+SELECT CategoryName, Description 
+FROM Categories
 
 /* Q3 */
 
-SELECT FirstName, LastName, HireDate FROM Employees 
+SELECT FirstName, LastName, HireDate 
+FROM Employees 
 WHERE Title = 'Sales Representative'
 
 /* Q4 */
 
-SELECT FirstName, LastName, HireDate FROM Employees 
+SELECT FirstName, LastName, HireDate 
+FROM Employees 
 WHERE Title = 'Sales Representative' AND Country = 'USA'
 
 /* Q5 */
 
-SELECT OrderID, OrderDate FROM Orders
+SELECT OrderID, OrderDate 
+FROM Orders
 WHERE EmployeeID = 5
 
 /* Q6 */
 
-SELECT SupplierID, ContactName, ContactTitle FROM Suppliers
+SELECT SupplierID, ContactName, ContactTitle 
+FROM Suppliers
 WHERE ContactTitle != 'Marketing Manager'
 
 /* Q7 */
 
-SELECT ProductID, ProductName FROM Products
+SELECT ProductID, ProductName 
+FROM Products
 WHERE ProductName LIKE '%Queso%'
 
 /* Q8 */
 
-SELECT OrderID, CustomerID, ShipCountry FROM Orders
+SELECT OrderID, CustomerID, ShipCountry 
+FROM Orders
 WHERE ShipCountry = 'France' OR ShipCountry = 'Belgium'
 
 /* Q9 */
 
-SELECT OrderID, CustomerID, ShipCountry FROM Orders
+SELECT OrderID, CustomerID, ShipCountry 
+FROM Orders
 WHERE  ShipCountry IN ('Brazil', 'Mexico', 'Argentina', 'Venezuela')
 
 /* Q10 */
 
+SELECT FirstName, LastName, Title, BirthDate 
+FROM Employees
+ORDER BY BirthDate
+
+/* Q11 */
+
+SELECT FirstName, LastName, Title, CAST(BirthDate as Date) DateOnlyBirthDate 
+FROM Employees
+ORDER BY BirthDate
+
+/* Q12 */
+
+SELECT e.FirstName, e.LastName, e.FirstName + ' ' + e.LastName FullName
+FROM Employees e
+
+/* Q13 */
+
+SELECT od.OrderID, od.ProductID, od.UnitPrice, od.Quantity, FORMAT(od.UnitPrice * od.Quantity, 'N') TotalPrice
+FROM OrderDetails od
+ORDER BY od.OrderID, od.ProductID
+
+/* Q14 */
+
+SELECT COUNT(*) TotalCustomers 
+FROM Customers
+
+/* Q15 */
+
+SELECT TOP 1 OrderDate FirstOrder
+FROM Orders
+ORDER BY OrderDate
+
+/* Q16 */
+
+SELECT DISTINCT(c.Country)
+FROM Customers c
