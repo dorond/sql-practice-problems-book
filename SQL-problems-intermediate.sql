044b73cb887309adbb7b957b550d2245e79364ed
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-11-21 21:39:40 +0200

    added q20 from intermediate

---
+++ SQL-problems-intermediate.sql
@@ -0,0 +1,15 @@
+/* Q20 */
+
+Select 
+    CategoryName,
+    TotalProducts = Count(*) 
+From 
+    Products
+Inner Join 
+    Categories
+        On Products.CategoryID = Categories.CategoryID
+Group By
+    CategoryName
+Order By 
+    Count(*) Desc
+
