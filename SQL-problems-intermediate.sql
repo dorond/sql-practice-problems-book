997eef75d5b99dbe266d9d80aa9738d63610e05a
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-11-21 22:16:46 +0200

    completed up to ex 22

--- SQL-problems-intermediate.sql
+++ SQL-problems-intermediate.sql
@@ -13,3 +13,31 @@ Group By
 Order By 
     Count(*) Desc
 
+/* Q21 */
+
+Select 
+    Country, 
+    City,
+    TotalCustomers = Count(*)
+
+From 
+    Customers
+Group BY
+    Country, 
+    City   
+Order BY
+    TotalCustomers Desc
+
+/* Q22 */
+
+Select 
+    ProductId, 
+    ProductName, 
+    UnitsInStock, 
+    ReorderLevel
+From 
+    Products
+Where 
+    UnitsInStock < ReorderLevel
+Order BY
+    ProductID
